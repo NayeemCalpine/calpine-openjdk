a97a916c684ad8a974a49a66b5171d35e679a4c6
Sambo Chea <sombochea@cubetiqs.com>
2021-07-14 21:36:39 +0700

    Fixed script dir for dockerfile

--- 11/build.sh
+++ 11/build.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
+SCRIPT_DIR=`dirname $0`
 C_ORG=cubetiq
 C_IMG=calpine-openjdk
 C_TAG=11
 C_ROOT=${C_ORG}/${C_IMG}:${C_TAG}
 
 echo "Starting build for OpenJDK: ${C_ROOT}"
-docker build . -t ${C_ROOT}
+docker build -f $SCRIPT_DIR/Dockerfile  . -t ${C_ROOT}
 
 echo "Publishing to DockerHub with ${C_ROOT}"
 docker push ${C_ROOT}
